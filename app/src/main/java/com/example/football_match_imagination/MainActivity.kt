b22bb0c94b4e0e9b4e09ef8ca29a3629171804cd
sw <sjugas1234@naver.com>
2023-11-12 09:49:30 +0900

    init

---
+++ app/src/main/java/com/example/football_match_imagination/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.football_match_imagination
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
