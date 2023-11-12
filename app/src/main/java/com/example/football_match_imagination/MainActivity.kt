9471f0c1bafa70e5158499d5a47f05dac18f959f
sw <sjugas1234@naver.com>
2023-11-12 13:33:25 +0900

    1차 로그인 UI

--- app/src/main/java/com/example/football_match_imagination/MainActivity.kt
+++ app/src/main/java/com/example/football_match_imagination/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.football_match_imagination
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.football_match_imagination.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
