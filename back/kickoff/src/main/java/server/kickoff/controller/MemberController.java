35ce992068f15aa4a2eeaf9f4ce1b764e5b44c37
apfhd12gk <apfhd12gk@naver.com>
2023-11-14 20:09:30 +0900

    feat(MemberController): 회원가입 정보를 받아 서비스계층으로 전달

--- back/kickoff/src/main/java/server/kickoff/controller/MemberController.java
+++ back/kickoff/src/main/java/server/kickoff/controller/MemberController.java
@@ -1,9 +1,43 @@
 package server.kickoff.controller;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import server.kickoff.domain.Member;
+import server.kickoff.repository.MemberJoinDto;
+import server.kickoff.service.MemberService;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/kick-off")
 public class MemberController {
 
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public CreateMemberResponse joinMember(@RequestBody MemberJoinDto joinDto) {
+        Member member = new Member();
+        member.settingMemberInfo( joinDto.getUserId(),
+                joinDto.getPassword(),
+                joinDto.getUserName(),
+                joinDto.getGender(),
+                joinDto.getAge(),
+                joinDto.getPhoneNumber(),
+                joinDto.getPosition());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class CreateMemberResponse {
+        private Long id;
+    }
 }
