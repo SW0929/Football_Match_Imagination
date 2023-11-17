5c3fb1c88e4ec35cc935818a04e738637b0f8df5
apfhd12gk <apfhd12gk@naver.com>
2023-11-17 16:45:16 +0900

    feat: 로그인 성공 또는 실패시 그에 맞는 응답 객체 반환

--- back/kickoff/src/main/java/server/kickoff/controller/LoginController.java
+++ back/kickoff/src/main/java/server/kickoff/controller/LoginController.java
@@ -1,6 +1,8 @@
 package server.kickoff.controller;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,17 +22,25 @@ public class LoginController {
     private final MemberService memberService;
 
     @PostMapping("/user/login")
-    public Object memberLogin(@RequestBody @Valid LoginForm form, BindingResult bindingResult) {
+    public LoginMemberResponse memberLogin(@RequestBody @Valid LoginForm form, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             // 에러 응답 JSON 반환 로직
         }
         Optional<Member> optionalMember = memberService.memberLogin(form.getLoginId(), form.getPassword());
 
         if (optionalMember.isPresent()) {
-            // 로그인 성공 로직
-            return
+            return new LoginMemberResponse(true, "Login successful");
         }
 
         //로그인 실패 로직
+        return new LoginMemberResponse(false, "Login fail");
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class LoginMemberResponse {
+        private boolean status;
+        private String message;
     }
 }
