be7ab3cb2eb02c42417839b2a6a998fd4e6867ee
apfhd12gk <apfhd12gk@naver.com>
2023-11-22 14:36:56 +0900

    feat: 회원가입시 아이디란 공백 검증

--- back/kickoff/src/main/java/server/kickoff/repository/MemberJoinDto.java
+++ back/kickoff/src/main/java/server/kickoff/repository/MemberJoinDto.java
@@ -1,10 +1,11 @@
 package server.kickoff.repository;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class MemberJoinDto {
-
+    @NotBlank(message = "아이디에 공백이 있으면 안됩니다.")
     private String userId;
 
     private String password;
