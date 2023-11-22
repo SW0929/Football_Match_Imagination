f45f7277f963d97e95a9341ae9df4d92b1a4e031
apfhd12gk <apfhd12gk@naver.com>
2023-11-22 14:42:22 +0900

    feat: 중복된 아이디 생성 검증 로직

--- back/kickoff/src/main/java/server/kickoff/service/MemberService.java
+++ back/kickoff/src/main/java/server/kickoff/service/MemberService.java
@@ -2,9 +2,12 @@ package server.kickoff.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import server.kickoff.domain.Member;
+import server.kickoff.exception.DuplicationException;
 import server.kickoff.repository.MemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,10 +16,18 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     public Long join(Member member) {
-        // 중복 확인 로직?
+        validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> members = memberRepository.findByUserId(member.getUserId());
+        if(!members.isEmpty()){
+            throw new DuplicationException("이미 존재하는 아이디 입니다.");
+        }
     }
 
     public Optional<Member> memberLogin(String loginId, String passwordId) {
