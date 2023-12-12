586ad99345b6105583890d7798d0c769d35a6aa4
apfhd12gk <apfhd12gk@naver.com>
2023-12-12 14:36:59 +0900

    refactor: Member 엔티티 매핑

--- back/kickoff/src/main/java/server/kickoff/domain/Member.java
+++ back/kickoff/src/main/java/server/kickoff/domain/Member.java
@@ -1,30 +1,41 @@
 package server.kickoff.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Timestamp;
 @Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "user_id")
     private String userId;
 
     private String password;
 
+    @Column(name = "user_name")
     private String userName;
 
     private String gender;
 
     private Integer age;
 
+    @Column(name = "phone_number")
     private String phoneNumber;
 
-
+    @CreatedDate
     private Timestamp createDate;
 
     public Member() {
-        this.createDate = new Timestamp(System.currentTimeMillis());
+
     }
 
     public void settingMemberInfo(String userId, String password, String userName, String gender, Integer age, String phoneNumber) {
